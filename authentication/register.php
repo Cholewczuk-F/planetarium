40726d3d7b77b038945c101c6db02134a6ac18fc
cholewczuk-f <thesarlysec@gmail.com>
2020-11-17 01:25:18 +0100

    updated register for user_img as default int fk from str

--- authentication/register.php
+++ authentication/register.php
@@ -18,10 +18,8 @@
         //escapes special characters in a string
         $user_login = mysqli_real_escape_string($con, $user_login);
 
-        $role_id = 3;
-
-        $user_img = 'blank_avatar.jpg';
-        $user_img = mysqli_real_escape_string($con, $user_img);
+        $role_id = 3; // default user role id
+        $user_img = 1; // default avatar img id
 
         $email    = stripslashes($_REQUEST['email']);
         $email    = mysqli_real_escape_string($con, $email);

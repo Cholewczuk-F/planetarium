92218f840025bbb1865db123cb661635f078ba67
cholewczuk-f <thesarlysec@gmail.com>
2020-11-29 23:49:11 +0100

    updates to post creation page and css

---
+++ posts/create.php
@@ -0,0 +1,30 @@
+<?php
+include_once('../config.php');
+require(SITE_ROOT . 'db\db_connect.php');
+include(SITE_ROOT . 'templates\header.php');
+?>
+<div id="page_content">
+    <div id="content_heading">
+        <h1>Dodaj nowy post</h1>
+    </div>
+
+    <div id="content">
+        <form name="addPost" action="POST" style="align: center;">
+            <table>
+                <th colspan="2">
+                    Prywatyzacja
+                </th>
+                <tr>
+                    <td style="width: 10%;">
+                        Tytuł:
+                    </td>
+                    <td>
+                        hehe
+                    </td>
+                </tr>
+            </table>
+        </form>
+    </div>
+    </body>
+
+    </html>

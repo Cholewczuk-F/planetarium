81d8ff5c9a52459ae6340da24cafb90a4fa6c030
Swagnar <derkarus@gmail.com>
2020-11-29 23:11:05 +0100

    bodiesIndex.php init and commentary for admin css

---
+++ pages/bodiesIndex.php
@@ -0,0 +1,29 @@
+<?php
+include('../db/db_connect.php');
+include('../config.php');
+include('../templates/header.php');
+
+?>
+
+<html>
+<head>
+    <title>Galeria</title>
+</head>
+<body>
+<div class = "main_container">
+    <div class = "gallery_container">
+        <div class = "gallery_thumbnail">
+        </div>
+        <div class = "gallery_name">
+        </div>
+        <div class = "gallery_objectCount">
+        </div>
+        <div class = "gallery_button">
+        </div>
+    </div>
+</div>
+</body>
+
+
+
+</html>

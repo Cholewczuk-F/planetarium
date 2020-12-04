dc232c1b9df611f44458677174fa0d1230ae6748
cholewczuk-f <thesarlysec@gmail.com>
2020-12-04 15:15:41 +0100

    blocked creating duplicate accounts

--- authentication/register.php
+++ authentication/register.php
@@ -3,8 +3,11 @@ include_once('../config.php');
 require(SITE_ROOT . 'db\db_connect.php');
 include(SITE_ROOT . 'templates\header.php');
 
+$err = array('user_login' => '', 'email' => '', 'password' => '', 'add' => '');
+
 // When form submitted, insert values into the database.
-if (isset($_REQUEST['user_login'])) {
+if (isset($_POST['submit'])) {
+
     // removes backslashes
     $user_login = stripslashes($_REQUEST['user_login']);
     //escapes special characters in a string
@@ -13,42 +16,57 @@ if (isset($_REQUEST['user_login'])) {
     $role_id = 3; // default user role id
     $user_img = 1; // default avatar img id
 
-    $email    = stripslashes($_REQUEST['email']);
-    $email    = mysqli_real_escape_string($con, $email);
+    $email = stripslashes($_REQUEST['email']);
+    $email = mysqli_real_escape_string($con, $email);
 
     $password = stripslashes($_REQUEST['password']);
     $password = mysqli_real_escape_string($con, $password);
 
     $create_date = date("Y-m-d H:i:s");
 
-    $query    = "INSERT into `users` (user_login, email, role_id, user_img, user_hash, create_date)
-                     VALUES ('$user_login', '$email', '$role_id', '$user_img', '" . md5($password) . "', '$create_date')";
-    $result   = mysqli_query($con, $query);
+    $query = "SELECT COUNT(*) FROM users WHERE user_login='$user_login' OR email='$email'";
+    $user_duplicates_n = mysqli_fetch_all(mysqli_query($con, $query));
 
-    if ($result) {
-        echo '<div class="form">
-                  <h3>Zarejestrowałeś się!</h3><br/>
-                  <p class="link">Nacisnij tutaj aby się <a href="' . SITE_URL_ROOT . 'authentication/login.php">Zalogować</a></p>
-                  </div>';
-    } else {
-        echo '<div class="form">
-                  <h3>Niepoprawnie wprowadzono dane.</h3><br/>
-                  <p class="link">Nacisnij tutaj aby się <a href="' . SITE_URL_ROOT . 'authentication/registration.php">zarejestrować</a> ponownie.</p>
-                  </div>';
+    $data_good = true;
+    if ($user_login == '') {
+        $data_good = false;
+        $err['user_login'] = '<p style="color:red;">Login nie może być pusty.</p>';
     }
-} else {
+    if ($password == '') {
+        $data_good = false;
+        $err['password'] = '<p style="color:red;">Hasło nie może być puste.</p>';
+    }
+    if ($email == '') {
+        $data_good = false;
+        $err['email'] = '<p style="color:red;">Email nie może być pusty.</p>';
+    }
+    if ($user_duplicates_n[0][0] != 0) {
+        $data_good = false;
+        $err['add'] = '<h3 style="color:red;">Login lub email zajęty.</h3><br/>';
+    }
+
+
+    if ($data_good == true) {
+        $query = "INSERT into `users` (user_login, email, role_id, user_img, user_hash, create_date)
+                     VALUES ('$user_login', '$email', '$role_id', '$user_img', '" . md5($password) . "', '$create_date')";
+        $result = mysqli_query($con, $query);
+        $err['add'] = '<h3>Zarejestrowałeś się!</h3><br/><p class="link">Nacisnij tutaj aby się <a href="' . SITE_URL_ROOT . 'authentication/login.php">Zalogować</a></p>';
+    }
+}
 ?>
+
 <form class="form" action="" method="post">
     <h1 class="login-title">Rejestracja</h1>
-    <input type="text" class="login-input" name="user_login" placeholder="Login" required />
+    <input type="text" class="login-input" name="user_login" placeholder="Login" />
+    <?php echo ($err['user_login']); ?>
     <input type="text" class="login-input" name="email" placeholder="Adres Email">
+    <?php echo ($err['email']); ?>
     <input type="password" class="login-input" name="password" placeholder="Hasło">
+    <?php echo ($err['password']); ?>
     <input type="submit" name="submit" value="Register" class="login-button">
     <p class="link"><a href="<?php echo SITE_URL_ROOT; ?>authentication/login.php">Zaloguj Się</a></p>
 </form>
-<?php
-}
-?>
+<?php echo ($err['add']); ?>
 </body>
 
 </html>

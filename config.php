76dbfd85e6193ed4e37cb2133fb04083c605d7a8
cholewczuk-f <thesarlysec@gmail.com>
2020-12-22 20:05:22 +0100

    eh co sie dzieje? dodawanie postów? weryfikacje? dlaczego ja żyje tutaj

--- config.php
+++ config.php
@@ -3,5 +3,8 @@ $root = pathinfo($_SERVER['SCRIPT_FILENAME']);
 define('PROJECT_NAME', 'planetarium');
 define('BASE_FOLDER', basename($root['dirname'])); // planetarium/posts/ => 'posts'
 define('SITE_ROOT',    realpath(dirname(__FILE__)) . '/'); // C:\xampp\htdocs\planetarium/
+
 define('BASE_URL',    'http://' . $_SERVER['HTTP_HOST'] . '/' . PROJECT_NAME . '/' . BASE_FOLDER . '/'); // http://localhost/planetarium/posts/
-define('SITE_URL_ROOT', 'http://' . $_SERVER['HTTP_HOST'] . '/' . PROJECT_NAME . '/'); // https://localhost/planetarium/posts/create.php
+define('SITE_URL_ROOT', 'http://' . $_SERVER['HTTP_HOST'] . '/' . PROJECT_NAME . '/'); // https://localhost/planetarium/
+
+define('POSTS_IMAGES', 'img/posts/');
--- posts/create.php
+++ posts/create.php
@@ -7,21 +7,76 @@ include(SITE_ROOT . 'utils.php');
 ?>
 
 <?php
-$query = "SELECT category_ID, name FROM categories";
-$categories = mysqli_fetch_all(mysqli_query($con, $query));
+function cancel_post($con, $post_id)
+{
+    $con->query("DELETE FROM posts WHERE post_ID LIKE $post_id");
+    print_r("DELETE FROM posts WHERE post_ID LIKE $post_id");
+}
+
+function cancel_gallery($con, $gallery_id)
+{
+    $con->query("DELETE FROM galleries WHERE gallery_ID LIKE $gallery_id");
+}
+
+function cancel_photos($con, $photos)
+{
+    foreach ($photos as $photo) {
+        if (file_exists($photo['path'])) {
+            print_r("delete: " . $photo['path']);
+            unlink($photo['path']);
+        }
+    }
+}
+
+$categories = mysqli_fetch_all(
+    $con->query(
+        "SELECT category_ID, name FROM categories"
+    )
+);
+$bodies = mysqli_fetch_all(
+    $con->query(
+        "SELECT body_ID, name FROM celestialbodies"
+    )
+);
 
 $errs = array(
     'title' => '',
     'content' => '',
     'category' => '',
+    'files' => '',
     'additional' => ''
 );
+
 $title = isset($_REQUEST['title']) ? $_REQUEST['title'] : '';
 $content = isset($_REQUEST['content']) ? $_REQUEST['content'] : '';
 $category_id = isset($_REQUEST['category']) ? $_REQUEST['category'] : '';
+$body_id = isset($_REQUEST['body']) ? $_REQUEST['body'] : '';
+
+// if new body => post body_id=null, redirect to new body with setting user_session 'new_body' as true, when completed update post
+$new_body = '';
+$isPostMeta = '';
+if ($body_id == 'new') {
+    $new_body = true;
+} else if ($body_id == 'meta') {
+    $isPostMeta = true;
+}
 
 if (isset($_POST['submit'])) {
-    if ($title != '' && $content != '' && $category_id != 'null') {
+
+    //check whether is there any file uploaded, and if any is from selected formats
+    $uploadable_files = '';
+    $formats = array("jpeg", "jpg", "png");
+    if (is_uploaded_file($_FILES['files']['tmp_name']['0'])) {
+        foreach ($_FILES['files']['tmp_name'] as $key => $tmp_name) {
+            $ext = pathinfo($_FILES["files"]["name"][$key], PATHINFO_EXTENSION);
+            if (in_array($ext, $formats)) {
+                $uploadable_files = true;
+            }
+        }
+    }
+
+    // rules: no new planet without images, no post without category, content or title
+    if ($title != '' && $content != '' && $category_id != 'null' && (!$new_body || $new_body && $uploadable_files)) {
         $user_login = $_SESSION['login'];
 
         $query = "SELECT user_ID FROM users WHERE user_login='$user_login'";
@@ -34,30 +89,123 @@ if (isset($_POST['submit'])) {
         $content = mysqli_real_escape_string($con, $content);
         $category_id = stripslashes($category_id);
         $category_id = mysqli_real_escape_string($con, $category_id);
+        $body_id = stripslashes($body_id);
+        $body_id = mysqli_real_escape_string($con, $body_id);
 
-        $body_id = 1; // default for now
-
-        // columns: post_ID(auto), title, content, user_id, category_id, body_id, created_at(auto)
-        if ($con->query(
-            "INSERT INTO posts VALUES (
-                null, 
-                '$title', 
-                '$content', 
-                '$user_id', 
-                '$category_id', 
-                '$body_id', 
-                null)
-            "
-        )) {
-            header('Location: ' . SITE_URL_ROOT . 'posts/post.php?id=' . $con->insert_id);
-            exit();
+        // queries differ in $body_id -> value or NULL (not passable in variable to mysqli)
+        if ($new_body || $isPostMeta) {
+            $sql_insert = "INSERT INTO posts VALUES (
+                null,
+                '$title',
+                '$content',
+                '$user_id',
+                '$category_id',
+                null,
+                null )";
         } else {
-            $errs['additional'] = '<p style="color:red; font-size:12px;">Problem z dodaniem posta. Proszę spróbować ponownie później.</p>';
+            $sql_insert = "INSERT INTO posts VALUES (
+                null,
+                '$title',
+                '$content',
+                '$user_id',
+                '$category_id',
+                '$body_id',
+                null )";
         }
 
-        // $insert_id = $con->insert_id;
-        // print_r("insert id:" . $insert_id);
+        do {
+            $post_id = $gallery_id = '';
 
+            if ($con->query($sql_insert)) {
+                $post_id = $con->insert_id;
+            } else {
+                $errs['additional'] = '<p style="color:red; font-size:12px;">Err 1: Problem z dodaniem posta. Proszę spróbować ponownie później.</p>';
+                break;
+            }
+
+            if ($uploadable_files) {
+                $photos = array();
+
+                // CREATE GALLERY GROUP TO NEWLY INSERTED POST
+                if ($con->query("INSERT INTO galleries VALUES(null, $post_id)")) {
+                    $gallery_id = $con->insert_id;
+                } else {
+                    // delete post at this point
+                    cancel_post($con, $post_id);
+
+                    $errs['additional'] = '<p style="color:red; font-size:12px;">Err 2: Problem z dodaniem posta. Proszę spróbować ponownie później.</p>' . $post_id;
+                    break;
+                }
+
+                // PROCESS PHOTO FILENAMES, PATHS DATA
+                foreach ($_FILES['files']['tmp_name'] as $key => $tmp_name) {
+                    $file_name = $_FILES['files']['name'][$key];
+                    $file_tmp = $_FILES['files']['tmp_name'][$key];
+                    $ext = pathinfo($file_name, PATHINFO_EXTENSION);
+
+                    if (in_array($ext, $formats)) {
+                        if (!file_exists(SITE_ROOT . POSTS_IMAGES . $file_name)) {
+
+                            array_push($photos, array(
+                                'move_from' => $_FILES["files"]["tmp_name"][$key],
+                                'name' => $file_name,
+                                'move_to' => POSTS_IMAGES . $file_name
+                            ));
+                        } else {
+
+                            $filename = basename($file_name, $ext);
+                            $newFilename = $filename . time() . "." . $ext;
+
+                            array_push($photos, array(
+                                'move_from' => $_FILES["files"]["tmp_name"][$key],
+                                'name' => $newFilename,
+                                'move_to' => POSTS_IMAGES . $newFilename
+                            ));
+                        }
+                    }
+                }
+
+                // CREATE PHOTOS ENTRIES FOR GALLERY
+                print_r($photos);
+
+                $photos_added = array();
+                foreach ($photos as $photo) {
+
+                    if ($con->query(
+                        "INSERT INTO images VALUES(
+                        null,
+                        $gallery_id, "
+                            . "'" . $photo['name'] . "',"
+                            . "'" . POSTS_IMAGES . $photo['name'] . "')"
+                    )) {
+                        $errs['additional'] = '';
+
+                        array_push($photos_added, array('id' => $con->insert_id, 'path' => SITE_ROOT . $photo['move_to']));
+                        move_uploaded_file($photo['move_from'], SITE_ROOT . $photo['move_to']);
+                    } else {
+                        // cancel operation
+
+                        // this order is dictated by foreign key restraints
+                        cancel_photos($con, $photos_added);
+                        cancel_gallery($con, $gallery_id);
+                        cancel_post($con, $post_id);
+
+                        $errs['additional'] = '<p style="color:red; font-size:12px;">Err 3: Problem z dodaniem posta. Proszę spróbować ponownie później.</p>';
+                        break;
+                    }
+                }
+                cancel_photos($con, $photos_added);
+            }
+
+            // // REDIRECT USER
+            // if ($new_body) {
+            //     header('Location: ' . SITE_URL_ROOT . 'planets/create.php?post_id=' . $post_id);
+            //     exit();
+            // } else {
+            //     header('Location: ' . SITE_URL_ROOT . 'posts/post.php?id=' . $post_id);
+            //     exit();
+            // }
+        } while (0);
     } else {
 
         // error setting
@@ -70,8 +218,12 @@ if (isset($_POST['submit'])) {
         if ($_REQUEST['category'] == 'null') {
             $errs['category'] = '<p style="color:red; font-size:12px;">Należy wybrać kategorię.</p>';
         }
+        if ($new_body && !$uploadable_files) {
+            $errs['files'] = '<p style="color:red; font-size:12px;">Należy załączyć zdjęcia.</p>';
+        }
+
+        // nowa planeta = bez zdj
     }
-} else {
 }
 ?>
 
@@ -82,7 +234,7 @@ if (isset($_POST['submit'])) {
     </div>
 
     <div id="content">
-        <form method="POST" action="create.php" style="align: center;">
+        <form method="POST" action="create.php" style="align: center;" enctype="multipart/form-data">
             <table>
                 <th colspan="2">
                     Dodaj Post
@@ -92,8 +244,7 @@ if (isset($_POST['submit'])) {
                         Tytuł:
                     </td>
                     <td>
-                        <input type="TEXT" name="title" placeholder="Tytuł" style="width: 98%; height: 24px;"
-                            value="<?php echo $title; ?>"></input>
+                        <input type="TEXT" name="title" placeholder="Tytuł" style="width: 98%; height: 24px;" value="<?php echo $title; ?>"></input>
                         <?php echo ($errs['title']); ?>
                     </td>
                 </tr>
@@ -102,22 +253,14 @@ if (isset($_POST['submit'])) {
                         Opisz post:
                     </td>
                     <td>
-                        <input type="TEXT" name="content" placeholder="Treść" style="width: 98%; height: 124px;"
-                            value="<?php echo $content; ?>"></input>
+                        <input type="TEXT" name="content" placeholder="Treść" style="width: 98%; height: 124px;" value="<?php echo $content; ?>"></input>
                         <?php echo ($errs['content']); ?>
-                    </td>
-                </tr>
-                <tr>
-                    <td colspan="2">
-                        <p>Załącz Zdjęcia <small>(Lub umieść post meta bez): </small></p>
-                        <br>
-                        <!-- <input type="file" name="file">-->
                     </td>
                 </tr>
                 <tr>
                     <td COLSPAN="2">
                         Wybierz kategorię:
-                        <select name="category" selected="<?php echo ($category_name); ?>">
+                        <select name="category">
                             <option value="null">(wybierz)</option>
                             <?php
                             foreach ($categories as $cat_arr) {
@@ -133,12 +276,44 @@ if (isset($_POST['submit'])) {
                     </td>
                 </tr>
                 <tr>
+                    <td colspan="2">
+                        <p>Wybierz Ciało Niebieskie Której Post Dotyczy<br>
+                        </p>
+                        <br>
+                        <select name="body">
+                            <option value="meta">( meta )</option>
+                            <option value="new" <?php if ($new_body == 1) {
+                                                    echo ('selected');
+                                                } ?>>( nowe ciało niebieskie )</option>
+                            <?php
+                            foreach ($bodies as $bod_arr) {
+                                echo ("<option value=\"" . $bod_arr[0] . "\"");
+                                if ($bod_arr[0] == $body_id) {
+                                    echo ("selected");
+                                }
+                                echo (">" . $bod_arr[1] . "</option>");
+                            }
+                            ?>
+                        </select>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <p>Załącz Zdjęcia, lub napisz meta:</br>
+                            <small>(Opcjonalne, wspierane: .jpeg .jpg .png)</small>
+                        </p>
+                        <br>
+                        <input type="file" name="files[]" multiple />
+                        <?php echo ($errs['files']); ?>
+                    </td>
+                </tr>
+                <tr>
                     <td colspan="2" style="height: 24px;">
-                        <input type="SUBMIT" name="submit" value="Dodaj Post" style="width: 15%;">
+                        <input type="SUBMIT" name="submit" value="Dodaj Post">
                     </td>
                 </tr>
             </table>
-        </form>
+        </form></br></br></br>
     </div>
     </body>
 
